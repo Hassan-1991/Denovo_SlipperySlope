4307d63c6e8f7d77c4ea6c330127a5e28369e41e
Hassan-1991 <67057211+Hassan-1991@users.noreply.github.com>
2026-03-05 11:52:24 -0600

    Update developing_script3.sh

--- developing_script3.sh
+++ developing_script3.sh
@@ -15,8 +15,6 @@ grep "^$i " E.coli_pairwise_ANI.99.49.clusters.dedup; done | awk '++seen[$1] <= 
 
 /stor/scratch/Ochman/hassan/100724_Complete_Genomes/Ecoli_genomics
 
-
-
 mkdir -p Ecoli_nonred_genomes
 #mkdir -p Ecoli_nonred_gffs/genemarks2
 #mkdir -p Ecoli_nonred_gffs/prodigal
@@ -54,55 +52,14 @@ done | split -l10
 ls x* | sed "s/^/bash /g" > prodigal_run
 /stor/work/Ochman/hassan/tools/parallelize_run.sh prodigal_run
 
+#Genemarks2:
 
+for i in $(ls *_genomic.renamed.fna | cut -f1 -d "_")
+do
+dir="${i}_gms2"
+echo "mkdir -p \"$dir\" && cd \"$dir\" && /stor/work/Ochman/hassan/tools/gms2_linux_64/gms2.pl --seq \"../${i}_genomic.renamed.fna\" --genome-type bacteria --gcode 11 --output \"${i}_genemarks2.gff\" --format gff3"
+done > genemarks_run
 
+/stor/work/Ochman/hassan/tools/parallelize_run.sh genemarks_run
 
-
-
-awk '
-{
-  cluster=$1
-  file=$2
-  count[cluster]++
-  newname="C"cluster"G"count[cluster]"_genomic.fna_genemarks2.gff"
-  src="../Ecoli.460.gffs/genemarks2_gffs/"file
-  dst="Ecoli_nonred_gffs/genemarks2/"newname
-  print "cp \""src"\" \""dst"\""
-}
-' nonred_genomes.withclusters.txt | bash
-
-awk '
-{
-  cluster=$1
-  file=$2
-  count[cluster]++
-  newname="C"cluster"G"count[cluster]"_genomic.fna_prodigal.gff"
-  src="../Ecoli.460.gffs/prodigal_gffs/"file
-  dst="Ecoli_nonred_gffs/prodigal/"newname
-  print "cp \""src"\" \""dst"\""
-}
-' nonred_genomes.withclusters.txt | bash
-
-
-
-
-
-
-
-
-
-
-
-f="C1G1_genomic.fna"
-prefix=$(basename "$f" | cut -f1 -d "_")
-
-awk -v pfx="$prefix" '
-BEGIN{n=0}
-/^>/{
-  if (tolower($0) ~ /plasmid/) { n++; print ">" pfx "_plasmid_" n }
-  else                        { print ">" pfx "_chromosome" }
-  next
-}
-{print}
-' "$f" > "${f%.fna}.renamed.fna"
-
+#Move to appropriate directories
