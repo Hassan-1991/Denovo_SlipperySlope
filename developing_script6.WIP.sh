9d7dab865bc46db04f25da9addbab62680721b58
Hassan-1991 <67057211+Hassan-1991@users.noreply.github.com>
2026-03-15 08:02:57 -0500

    Update developing_script6.WIP.sh

--- developing_script6.WIP.sh
+++ developing_script6.WIP.sh
@@ -11,3 +11,8 @@
 #Pangenome, ORFs
 /stor/work/Ochman/hassan/E.coli_ORFan/E.coli_ORFan_pipeline_8-10/diamond blastp -q Ecoli_queries.prot.faa -d Ecoli.genome.pangenomedb.orfipy.dmnd --outfmt 6 qseqid sseqid pident nident qcovhsp length mismatch gapopen gaps qstart qend sstart send qlen slen evalue bitscore --ultra-sensitive --out all_proteins_vs_pangenome_ORFs.tsv -k 0 -b8 -c1
 
+cat all_proteins_vs_GBRS_annotated.tsv |
+grep -v -F -f /stor/work/Ochman/hassan/MS_Ecoli_ORFans_Ch3/rethinking_clustering/all_Escherichia_slippedby_IDs.txt - | #remove contaminants
+awk -F '\t' '($5>60&&$16<0.001)' | cut -f1 | sort -u > step1_genusspecific_nonORFan.txt
+
+/stor/work/Ochman/hassan/MS_Ecoli_ORFans_Ch3/rethinking_clustering/all_Escherichia_slippedby_IDs.txt
